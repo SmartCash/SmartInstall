f9197a6779bdc8849051ffecdcf11f0de737b473
thesolarminer <solarminer@smartcash.cc>
2020-08-08 01:42:12 -0400

    Update Upgrade Script

--- SmartUpgradeNode.sh
+++ SmartUpgradeNode.sh
@@ -3,12 +3,12 @@ echo 'Downloading and extracting wallet files'
 wget "https://github.com/SmartCash/Core-Smart/releases/download/v1.3.0/smartcash-1.3.0-x86_64-linux-gnu.tar.gz" -O - | tar -xz
 
 echo 'Stopping and removing previous version'
-~/smartcash-cli stop && smartcash-cli stop && sleep 10
+~/smartcash-cli stop && sleep 10
 rm ~/smartcashd 
 rm ~/smartcash-cli && sleep 1
 
 echo 'Copying files to home directory and setting permissions'
-cp smartcash-1.3.0/bin/smartcash ~/ 
+cp smartcash-1.3.0/bin/smartcashd ~/ 
 cp smartcash-1.3.0/bin/smartcash-cli ~/
 chmod a+x ~/smartcashd ~/smartcash-cli
 
