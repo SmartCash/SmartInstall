3a1cf749ff246161e9d548214d3a24866c4b841c
thesolarminer <thesolarminer@users.noreply.github.com>
2020-08-13 00:55:15 -0500

    Update SmartUpgradeNode.sh

--- SmartUpgradeNode.sh
+++ SmartUpgradeNode.sh
@@ -3,17 +3,17 @@ echo 'Downloading and extracting wallet files'
 wget "https://github.com/SmartCash/Core-Smart/releases/download/v1.3.0/smartcash-1.3.0-x86_64-linux-gnu.tar.gz" -O - | tar -xz
 
 echo 'Stopping and removing previous version'
-test -f ~/smartcash-cli && ~/smartcash-cli stop && sleep 20 && rm ~/smartcashd && rm ~/smartcash-cli && sleep 1
-test -f /usr/bin/smartcash-cli && smartcash-cli stop && sleep 20 && echo 'Please remove the old smartcashd with apt remove smartcashd' && sleep 1
+test -f ~/smartcash-cli && ~/smartcash-cli stop && sleep 15 && rm ~/smartcashd && rm ~/smartcash-cli && sleep 5
+test -f /usr/bin/smartcash-cli && smartcash-cli stop && sleep 10 && echo 'Please remove the old smartcashd with apt remove smartcashd' && sleep 10
 
 echo 'Copying files to home directory and setting permissions'
 cp smartcash-1.3.0/bin/smartcashd ~/ 
 cp smartcash-1.3.0/bin/smartcash-cli ~/
 chmod a+x ~/smartcashd ~/smartcash-cli
-echo 'sapi=1' >> ~/.smartcash/smartcash.conf
-~/smartcashd && sleep 10
+echo 'sapi=1' >> ~/.smartcash/smartcash.conf && sleep 2
+~/smartcashd && sleep 15
 echo 'A few more seconds and we will check.'
-sleep 10 && ~/smartcash-cli getinfo
+sleep 15 && ~/smartcash-cli getinfo
 
 echo 'Upgrade finished.'
 echo 'To stop wallet ./smartcash-cli stop'
