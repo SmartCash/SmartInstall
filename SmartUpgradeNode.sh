a578199189957b947325ce4124f767ee9af20e6c
thesolarminer <thesolarminer@users.noreply.github.com>
2020-08-13 00:47:02 -0500

    Update SmartUpgradeNode.sh

--- SmartUpgradeNode.sh
+++ SmartUpgradeNode.sh
@@ -3,17 +3,22 @@ echo 'Downloading and extracting wallet files'
 wget "https://github.com/SmartCash/Core-Smart/releases/download/v1.3.0/smartcash-1.3.0-x86_64-linux-gnu.tar.gz" -O - | tar -xz
 
 echo 'Stopping and removing previous version'
-~/smartcash-cli stop && sleep 20
-rm ~/smartcashd 
-rm ~/smartcash-cli && sleep 1
+test -f ~/smartcash-cli && ~/smartcash-cli stop && sleep 20 && rm ~/smartcashd && rm ~/smartcash-cli && sleep 1
+test -f /usr/bin/smartcash-cli && smartcash-cli stop && sleep 20 && echo 'Please remove the old smartcashd with apt remove smartcashd' && sleep 1
 
 echo 'Copying files to home directory and setting permissions'
 cp smartcash-1.3.0/bin/smartcashd ~/ 
 cp smartcash-1.3.0/bin/smartcash-cli ~/
 chmod a+x ~/smartcashd ~/smartcash-cli
-
-~/smartcashd && sleep 1
+echo 'sapi=1' >> ~/.smartcash/smartcash.conf
+~/smartcashd && sleep 10
+echo 'A few more seconds and we will check.'
+sleep 10 && ~/smartcash-cli getinfo
 
 echo 'Upgrade finished.'
 echo 'To stop wallet ./smartcash-cli stop'
 echo 'To start ./smartcashd'
+echo 'If you don't already have a keep alive script add this crontab entry to keep your SmartNode running if the server restarts or is accidentally stopped.'
+echo 'crontab -e  choose nano as an editor and enter at end of file'
+echo '*/5 * * * * pidof smartcashd || ~/smartcashd'
+echo 'Control x and y to save and quit"
