6cd962716681ea53679b3a64277938db2ce33167
thesolarminer <thesolarminer@users.noreply.github.com>
2021-01-26 12:57:09 -0600

    Update SmartInstallnoSAPI.sh

--- SmartInstallnoSAPI.sh
+++ SmartInstallnoSAPI.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Downloading the wallet
 echo 'Downloading and extracting wallet files'
-wget "https://github.com/SmartCash/Core-Smart/releases/download/v1.3.4/smartcash-1.3.4-x86_64-linux-gnu.tar.gz" -O - | tar -xz
+wget "https://github.com/SmartCash/Core-Smart/releases/download/v2.0.0/smartcash-1.3.4-x86_64-linux-gnu.tar.gz" -O - | tar -xz
 
 # Extract the files and give executable permissions
 echo 'Copying files to home directory'
-cp smartcash-1.3.4/bin/smartcashd ~/
-cp smartcash-1.3.4/bin/smartcash-cli ~/
+cp smartcash-2.0.0/bin/smartcashd ~/
+cp smartcash-2.0.0/bin/smartcash-cli ~/
 chmod a+x ~/smartcashd ~/smartcash-cli
 
 # Create wallet directory and set to run when windows is closed.
@@ -15,6 +15,8 @@ echo 'daemon=1' > ~/.smartcash/smartcash.conf
 echo 'sapi=0' >> ~/.smartcash/smartcash.conf
 echo '#smartnode=1' >> ~/.smartcash/smartcash.conf
 echo '#smartnodeprivkey=' >> ~/.smartcash/smartcash.conf
+echo '#You can use litemode if you don't need smartnode info.' >> ~/.smartcash/smartcash.conf
+echo '#litemode=1' >> ~/.smartcash/smartcash.conf
 
 # Start Wallet
 ./smartcashd && sleep 30 && ./smartcash-cli getinfo
@@ -24,8 +26,8 @@ echo '#smartnodeprivkey=' >> ~/.smartcash/smartcash.conf
 
 # Create a shortcut on system - Unhash if desired
 #echo 'Creating symbol link'
-#ln -sf ~/smartcash-1.3.4/bin/smartcashd /usr/bin/smartcashd
-#ln -sf ~/smartcash-1.3.4/bin/smartcash-cli /usr/bin/smartcash-cli
+#ln -sf ~/smartcash-2.0.0/bin/smartcashd /usr/bin/smartcashd
+#ln -sf ~/smartcash-2.0.0/bin/smartcash-cli /usr/bin/smartcash-cli
 
 echo 'Installation finished.'
 echo 'To stop wallet ./smartcash-cli stop'
