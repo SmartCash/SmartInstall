ab5610c61ef2e5861fb567d1483b3a38c2ed4ad0
thesolarminer <thesolarminer@users.noreply.github.com>
2021-02-01 10:47:13 -0600

    Update SmartUpgradeNode.sh

--- SmartUpgradeNode.sh
+++ SmartUpgradeNode.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo 'Downloading and extracting wallet files'
-wget "https://github.com/SmartCash/Core-Smart/releases/download/v1.3.4/smartcash-1.3.4-x86_64-linux-gnu.tar.gz" -O - | tar -xz
+wget "https://github.com/SmartCash/Core-Smart/releases/download/v2.0.0/smartcash-2.0.0-x86_64-linux-gnu.tar.gz" -O - | tar -xz
 
 echo 'Stopping and removing previous version'
 test -f ~/smartcash-cli && ~/smartcash-cli stop && sleep 15 && rm ~/smartcashd && rm ~/smartcash-cli && sleep 5
 test -f /usr/bin/smartcash-cli && smartcash-cli stop && sleep 10 && echo 'Please remove the old smartcashd with sudo apt remove smartcashd' && sleep 10
 
 echo 'Copying files to home directory and setting permissions'
-cp smartcash-1.3.4/bin/smartcashd ~/ 
-cp smartcash-1.3.4/bin/smartcash-cli ~/
+cp smartcash-2.0.0/bin/smartcashd ~/ 
+cp smartcash-2.0.0/bin/smartcash-cli ~/
 chmod a+x ~/smartcashd ~/smartcash-cli
 [[ -z $(grep sapi=1 ~/.smartcash/smartcash.conf) ]] && echo 'sapi=1' >> ~/.smartcash/smartcash.conf || echo "SAPI check complete" 
 echo "Going to reindex to clean to generate new rewards data."
@@ -19,15 +19,11 @@ echo "You can also use tail ~/.smartcash/debug.log if the getinfo doesn't respon
 echo 'Upgrade finished.'
 echo 'To stop wallet ./smartcash-cli stop'
 echo 'To start ./smartcashd'
+echo 'Use nano .smartcash/smartcash.conf to edit your config file.  Control x y to save."
+echo "Make sure your smartnodeprivkey matches your local wallet and issue a Start Alias from your local wallet.'
+echo "Change the smartnodeprivkey on both local and vps if a start doesn't get recognized on upgrades."
 echo "If you don't already have a keep alive script add this crontab entry to keep your SmartNode running if the server restarts or is accidentally stopped."
 echo 'Enter "crontab -e" and choose nano as an editor and enter the following line at end of file'
 echo '*/5 * * * * pidof smartcashd || ~/smartcashd'
 echo "Remove line with makerun script as this will reference the old version"
 echo 'Use Control x and y to save and quit'
-echo "To remove blocks if stuck or requested to get a clean sync" 
-echo "./smartcash-cli stop"
-echo "cd .smartcash"
-echo "only run the next command on a vps that doesn't have funds."
-echo "rm -r !(@(smartcash.conf|wallet.dat))"
-echo "cd .."
-echo "./smartcashd"
