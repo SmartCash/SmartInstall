a237ed7692f30e265594eda0b298a1dbf03c3dae
Solarminer <solarminer@smartcash.cc>
2021-07-25 15:35:54 -0500

    Update to 2.0.6

--- SmartInstallnoSAPI.sh
+++ SmartInstallnoSAPI.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Downloading the wallet
 echo 'Downloading and extracting wallet files'
-wget "https://github.com/SmartCash/Core-Smart/releases/download/v2.0.5/smartcash-2.0.5-x86_64-linux-gnu.tar.gz" -O - | tar -xz
+wget "https://github.com/SmartCash/Core-Smart/releases/download/v2.0.6/smartcash-2.0.6-x86_64-linux-gnu.tar.gz" -O - | tar -xz
 
 # Extract the files and give executable permissions
 echo 'Copying files to home directory'
-cp smartcash-2.0.5/bin/smartcashd ~/
-cp smartcash-2.0.5/bin/smartcash-cli ~/
+cp smartcash-2.0.6/bin/smartcashd ~/
+cp smartcash-2.0.6/bin/smartcash-cli ~/
 chmod a+x ~/smartcashd ~/smartcash-cli
 
 # Create wallet directory and set to run when windows is closed.
@@ -26,8 +26,8 @@ echo '#litemode=1' >> ~/.smartcash/smartcash.conf
 
 # Create a shortcut on system - Unhash if desired
 #echo 'Creating symbol link'
-#ln -sf ~/smartcash-2.0.5/bin/smartcashd /usr/bin/smartcashd
-#ln -sf ~/smartcash-2.0.5/bin/smartcash-cli /usr/bin/smartcash-cli
+#ln -sf ~/smartcash-2.0.6/bin/smartcashd /usr/bin/smartcashd
+#ln -sf ~/smartcash-2.0.6/bin/smartcash-cli /usr/bin/smartcash-cli
 
 echo 'Installation finished.'
 echo 'To stop wallet ./smartcash-cli stop'
